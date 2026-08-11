c0721b104fd82fc72a90abffb88c5f3cfa7dcd9b
Nágyla Silva <silvanagyla92@gmail.com>
2026-08-10 23:10:57 -0300

    Refactor test cases for product controller

--- tests/controllers/test_product.py
+++ tests/controllers/test_product.py
@@ -1,12 +1,16 @@
 from typing import List
 
 import pytest
-from tests.factories import product_data
 from fastapi import status
+
+from tests.factories import product_data
 
 
 async def test_controller_create_should_return_success(client, products_url):
-    response = await client.post(products_url, json=product_data())
+    response = await client.post(
+        products_url,
+        json=product_data(),
+    )
 
     content = response.json()
 
@@ -24,9 +28,13 @@ async def test_controller_create_should_return_success(client, products_url):
 
 
 async def test_controller_get_should_return_success(
-    client, products_url, product_inserted
+    client,
+    products_url,
+    product_inserted,
 ):
-    response = await client.get(f"{products_url}{product_inserted.id}")
+    response = await client.get(
+        f"{products_url}{product_inserted.id}"
+    )
 
     content = response.json()
 
@@ -43,17 +51,28 @@ async def test_controller_get_should_return_success(
     }
 
 
-async def test_controller_get_should_return_not_found(client, products_url):
-    response = await client.get(f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca")
+async def test_controller_get_should_return_not_found(
+    client,
+    products_url,
+):
+    response = await client.get(
+        f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+    )
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {
-        "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        "detail": (
+            "Product not found with filter: "
+            "4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        )
     }
 
 
 @pytest.mark.usefixtures("products_inserted")
-async def test_controller_query_should_return_success(client, products_url):
+async def test_controller_query_should_return_success(
+    client,
+    products_url,
+):
     response = await client.get(products_url)
 
     assert response.status_code == status.HTTP_200_OK
@@ -62,10 +81,13 @@ async def test_controller_query_should_return_success(client, products_url):
 
 
 async def test_controller_patch_should_return_success(
-    client, products_url, product_inserted
+    client,
+    products_url,
+    product_inserted,
 ):
     response = await client.patch(
-        f"{products_url}{product_inserted.id}", json={"price": "7.500"}
+        f"{products_url}{product_inserted.id}",
+        json={"price": "7.500"},
     )
 
     content = response.json()
@@ -83,20 +105,48 @@ async def test_controller_patch_should_return_success(
     }
 
 
-async def test_controller_delete_should_return_no_content(
-    client, products_url, product_inserted
+async def test_controller_patch_should_return_not_found(
+    client,
+    products_url,
 ):
-    response = await client.delete(f"{products_url}{product_inserted.id}")
+    response = await client.patch(
+        f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca",
+        json={"price": "7.500"},
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {
+        "detail": (
+            "Product not found with filter: "
+            "4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        )
+    }
+
+
+async def test_controller_delete_should_return_no_content(
+    client,
+    products_url,
+    product_inserted,
+):
+    response = await client.delete(
+        f"{products_url}{product_inserted.id}"
+    )
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
-async def test_controller_delete_should_return_not_found(client, products_url):
+async def test_controller_delete_should_return_not_found(
+    client,
+    products_url,
+):
     response = await client.delete(
         f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
     )
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {
-        "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        "detail": (
+            "Product not found with filter: "
+            "4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        )
     }
