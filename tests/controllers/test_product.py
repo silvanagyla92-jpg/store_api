20ae016fb490800cbec013d28e793c636a87bb79
Nágyla Silva <silvanagyla92@gmail.com>
2026-08-10 23:28:13 -0300

    Add delete controller tests for no content and not found

--- tests/controllers/test_product.py
+++ tests/controllers/test_product.py
@@ -179,6 +179,36 @@ async def test_controller_patch_should_allow_update_updated_at(
     )
 
 
+async def test_controller_delete_should_return_no_content(
+    client,
+    products_url,
+    product_inserted,
+):
+    response = await client.delete(
+        f"{products_url}{product_inserted.id}"
+    )
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+async def test_controller_delete_should_return_not_found(
+    client,
+    products_url,
+):
+    response = await client.delete(
+        f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    assert response.json() == {
+        "detail": (
+            "Product not found with filter: "
+            "4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
+        )
+    }
+
+
 @pytest.mark.usefixtures("products_inserted")
 async def test_controller_query_should_filter_by_min_price(
     client,
@@ -236,33 +266,3 @@ async def test_controller_query_should_filter_by_price_range(
         price = float(product["price"])
 
         assert 5000 <= price <= 8000
-
-
-async def test_controller_delete_should_return_no_content(
-    client,
-    products_url,
-    product_inserted,
-):
-    response = await client.delete(
-        f"{products_url}{product_inserted.id}"
-    )
-
-    assert response.status_code == status.HTTP_204_NO_CONTENT
-
-
-async def test_controller_delete_should_return_not_found(
-    client,
-    products_url,
-):
-    response = await client.delete(
-        f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
-    )
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-
-    assert response.json() == {
-        "detail": (
-            "Product not found with filter: "
-            "4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
-        )
-    }
